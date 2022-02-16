0ed62128fb14ac1bb6c71558feea27c2ee72a371
Sebastian Stöckl <sebastian.stoeckl@uni.li>
2022-02-16 03:21:32 +0000

    update install file. include command for installing ubuntu packages at the beginning

--- install.R
+++ install.R
@@ -1,27 +1,32 @@
+# sudo apt-get install xml2 libxml2-dev tcl8.6-dev tk tk8.6-dev libgsl2.3 glpk-utils libglpk-dev coinor-libcbc-dev coinor-libclp-dev
+# coinor-libsymphony-dev curl
 # initial packages
 install.packages("devtools")
 install.packages(c("pacman"))
-install.packages(c("shiny","shinyWidgets","shinydashboardPlus","shinycssloaders",))
+install.packages(c("shiny","shinyWidgets","shinydashboardPlus","shinycssloaders","DT"))
 install.packages(c("rmarkdown","blogdown","bookdown","knitr","kableExtra"))
+install.packages("BiocManager"); BiocManager::install("Rgraphviz");
 # tidy packages
 install.packages(c("tidyverse","tidyquant","tidymodels","furrr","slider","timetk","lubridate","tidyr","purrr","corrr",
                    "tibbletime","broom","vroom","glue"))
-install.packages(c("DT"))
+install.packages(c("data.table"))
 # finance packages
 install.packages(c("tsibble","PerformanceAnalytics","PortfolioAnalytics",
   "Quandl","alphavantager","GGally",
   "DEoptim","pso","GenSA","Rglpk","ROI","ROI.plugin.glpk","ROI.plugin.quadprog","doParallel"))
-install.packages(c("fPortfolio"), dependencies="Suggests")
-
+install.packages(c("Rsymphony","Rglpk","fPortfolio"))#, dependencies="Suggests")
+install.packages(c("PortfolioAnalytics"), dependencies="Suggests")
 # machine learning
 install.packages("keras")
-install.packages("caret", dependencies = "Suggests")
-install.packages(c("BART","CAM+","caTools","AUC","CausalImpact","cowplot","breakDown","dummies","e1071",
+install.packages("caret")#, dependencies = "Suggests")
+BiocManager::install("RBGL"); BiocManager::install("CAM")
+install.packages(c("BART","caTools","AUC","CausalImpact","cowplot","breakDown","dummies","e1071",
                    "factoextra","fastAdaboost","forecast","FNN","ggpubr","glmnet","iml","lime","naivebayes",
                    "pcalg","quadprog","quantmod","randomForest","rBayesianOptimization","R6","ReinforcementLearning",
                    "rpart","rpart.plot","spBayes","xgboost","nnet","kernlab","forest"))
-install.packages("BiocManager"); BiocManager::install("Rgraphviz")
-install.packages(c("nlopt"))
+devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
+install.packages("CompareCausalNetworks", dependencies = "Suggests")
+install.packages(c("nloptr"))
 # statistics
 install.packages(c("pracma","zoo","xts","timeSeries","timeDate"))
 install.packages(c("sandwich","lmtest","plm"))
@@ -29,12 +34,14 @@ install.packages(c("changepoint","changepoint.np","strucchange","cpm","ocp"))
 
 # sstoeckl personal packages
 install.packages("crypto2")
+install.packages("FFdownload")
 devtools::install_github("sstoeckl/uncertaintymeasures")
-devtools::install_github("sstoeckl/ffdownload")
-devtools::install_github("sstoeckl/diffform")
+devtools::install_github("sstoeckl/pensionfinanceLi")
 
 # latex etc.
-install.packages(c("xtable","maps","ggmap","mice","officer","devEMF","tis","plotly","ggthemes"))
+install.packages(c("xtable","maps","ggmap","mice","officer","devEMF","tis","plotly","ggthemes","RCurl"))
 install.packages(c("plyr","tictoc","beepr","tikzDevice","readxl","writexl","viridis"))
 install.packages(c("RcppEigen","RcppArmadillo","RcppParallel","RcppRoll"))
 # install.packages(c(""))
+
+
