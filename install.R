7a197b643ec7e9111df1c00673295df45384f710
Sebastian Stöckl <36445234+sstoeckl@users.noreply.github.com>
2024-09-10 13:53:29 +0200

    Update install.R
    Update (for R 4.4.1)

--- install.R
+++ install.R
@@ -1,60 +1,63 @@
-# sudo apt-get install xml2 libxml2-dev tcl8.6-dev tk tk8.6-dev libgsl2.3 glpk-utils libglpk-dev coinor-libcbc-dev coinor-libclp-dev
-# coinor-libsymphony-dev curl libmariadb-dev libmysqlclient-dev libssl-dev texlive texlive-latex-extra libpq-dev python3.8-venv python3.8-dev
-# tensorrt libpq-dev libssl-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev texlive-latex-base
-
-# locales: sudo dpkg-reconfigure locales (110 112 114 141 159)
-# initial packages
+# Install devtools and initial helper packages
 install.packages("devtools")
 install.packages(c("pacman"))
-install.packages(c("shiny","shinyWidgets","shinydashboardPlus","shinycssloaders","DT"))
-install.packages(c("rmarkdown","blogdown","bookdown","knitr","kableExtra"))
-install.packages("BiocManager"); BiocManager::install("Rgraphviz");
-# tidy packages
-install.packages(c("tidyverse","tidyquant","tidymodels","furrr","slider","timetk","lubridate","tidyr","purrr","corrr",
-                   "tibbletime","broom","vroom","glue","janitor"))
-install.packages(c("data.table"))
-# finance packages
-install.packages(c("tsibble","PerformanceAnalytics","PortfolioAnalytics",
-  "Quandl","alphavantager","GGally",
-  "DEoptim","pso","GenSA","Rglpk","ROI","ROI.plugin.glpk","ROI.plugin.quadprog","doParallel"))
-install.packages(c("Rsymphony","Rglpk","fPortfolio"))#, dependencies="Suggests")
-install.packages(c("PortfolioAnalytics"), dependencies="Suggests")
-# machine learning
-install.packages(c("tensorflow","reticulate","keras"))
-install.packages("caret")#, dependencies = "Suggests")
-BiocManager::install("RBGL"); BiocManager::install("CAM")
-install.packages(c("BART","caTools","AUC","CausalImpact","cowplot","breakDown","dummies","e1071",
-                   "factoextra","fastAdaboost","forecast","FNN","ggpubr","glmnet","iml","lime","naivebayes",
-                   "pcalg","quadprog","quantmod","randomForest","rBayesianOptimization","R6","ReinforcementLearning",
-                   "rpart","rpart.plot","spBayes","xgboost","nnet","kernlab","forest","torch","ForecastComb","tabnet","MultivariateRandomForest",
-                  "factoextra","NbClust"))
 
-install.packages(c("tidymodels")) #"parsnip","tune","rsample","recipes"))
+# Shiny and dashboard packages
+install.packages(c("shiny", "shinyWidgets", "shinydashboardPlus", "shinycssloaders", "DT"))
+
+# Markdown and reporting tools
+install.packages(c("rmarkdown", "blogdown", "bookdown", "knitr", "kableExtra"))
+
+# Bioconductor packages
+install.packages("BiocManager")
+BiocManager::install("Rgraphviz")
+
+# Tidyverse and related packages
+install.packages(c("tidyverse", "tidyquant", "tidymodels", "furrr", "slider", "timetk", "lubridate", "tidyr", "purrr", "corrr", "tibbletime", "broom", "vroom", "glue", "janitor"))
+
+# Finance-related packages
+install.packages(c("tsibble", "PerformanceAnalytics", "PortfolioAnalytics", "Quandl", "alphavantager", "GGally", "DEoptim", "pso", "GenSA", "Rglpk", "ROI", "ROI.plugin.glpk", "ROI.plugin.quadprog", "doParallel"))
+install.packages(c("Rsymphony", "Rglpk", "fPortfolio"))
+
+# Machine learning packages
+install.packages(c("tensorflow", "reticulate", "keras"))
+install.packages("caret")
+BiocManager::install(c("RBGL", "CAM"))
+install.packages(c("BART", "caTools", "AUC", "CausalImpact", "cowplot", "breakDown", "dummies", "e1071", "factoextra", "fastAdaboost", "forecast", "FNN", "ggpubr", "glmnet", "iml", "lime", "naivebayes", "pcalg", "quadprog", "quantmod", "randomForest", "rBayesianOptimization", "R6", "ReinforcementLearning", "rpart", "rpart.plot", "spBayes", "xgboost", "nnet", "kernlab", "forest", "torch", "ForecastComb", "tabnet", "MultivariateRandomForest", "factoextra", "NbClust"))
+
+# Additional machine learning tools
+install.packages("tidymodels")
 devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
-install.packages("CompareCausalNetworks", dependencies = "Suggests")
-install.packages(c("nloptr"))
-install.packages(c("mlr3","mlr3learners","mlr3verse"))
+install.packages("CompareCausalNetworks", dependencies = TRUE)
+install.packages("nloptr")
+install.packages(c("mlr3", "mlr3learners", "mlr3verse"))
 remotes::install_github("mlr-org/mlr3mbo")
-# statistics
-install.packages(c("pracma","zoo","xts","timeSeries","timeDate"))
-install.packages(c("sandwich","lmtest","plm"))
-install.packages(c("changepoint","changepoint.np","strucchange","cpm","ocp"))
+
+# Statistics and time series packages
+install.packages(c("pracma", "zoo", "xts", "timeSeries", "timeDate"))
+install.packages(c("sandwich", "lmtest", "plm"))
+install.packages(c("changepoint", "changepoint.np", "strucchange", "cpm", "ocp"))
 install.packages(c("BayesianFactorZoo"))
 
-# sstoeckl personal packages
+# Personal packages
 install.packages("crypto2")
-install.packages(c("FFdownload","frenchdata"))
+install.packages(c("FFdownload", "frenchdata"))
 devtools::install_github("sstoeckl/uncertaintymeasures")
 devtools::install_github("sstoeckl/pensionfinanceLi")
 
-# latex etc.
-install.packages(c("xtable","maps","ggmap","mice","officer","devEMF","tis","plotly","ggthemes","RCurl"))
-install.packages(c("plyr","tictoc","beepr","tikzDevice","readxl","writexl","viridis"))
-install.packages(c("RcppEigen","RcppArmadillo","RcppParallel","RcppRoll"))
-install.packages(c("RMariaDB","DBI","dbplyr","RPostgres"))
-install.packages(c("learnr","exams"))
-install.packages(c("doSNOW","RobStatTM","R.rsp"))
-install.packages(c("job"))
+# Latex, visualization, and reporting tools
+install.packages(c("xtable", "maps", "ggmap", "mice", "officer", "devEMF", "tis", "plotly", "ggthemes", "RCurl"))
+install.packages(c("plyr", "tictoc", "beepr", "tikzDevice", "readxl", "writexl", "viridis"))
 
+# Rcpp and other C++ bindings
+install.packages(c("RcppEigen", "RcppArmadillo", "RcppParallel", "RcppRoll"))
 
+# Database packages
+install.packages(c("RMariaDB", "DBI", "dbplyr", "RPostgres"))
 
+# Learning tools
+install.packages(c("learnr", "exams"))
+
+# Parallel computing and job management
+install.packages(c("doSNOW", "RobStatTM", "R.rsp"))
+install.packages("job")
